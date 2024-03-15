692d9183e2740f24d30b7427c87514aae0d5dcb5
mohadese asadi <mohiw.1380@gmail.com>
2024-03-16 01:00:29 +0330

    update style and fix js bug

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 const form = document.querySelector(".top-banner form")
 const input = document.querySelector(".top-banner input")
 const msg = document.querySelector(".top-banner .msg")
-const list = document.querySelector(".load-section")
+const list = document.querySelector(".cities")
 
-const apiKey = "f42eb562336bf63c8f83b03c7dd895fe";
+const apiKey = "d57d533492daa8e3150b19f9a19af17a";
 
 form.addEventListener("submit", e => {
     e.preventDefault();
     let inputVal = input.value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units={metric}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`
     fetch(url)
         .then(response => response.json())
         .then(data => {
             console.log(data)
-            const {main,name, sys , weather} = data;
+            const {main, name, sys, weather} = data;
             const li = document.createElement("li");
             li.classList.add("city");
             const markup = `
@@ -24,12 +24,14 @@ form.addEventListener("submit", e => {
             <div class="city-temp">${Math.round(main.temp)}</div>
             <figure>
                 <figcaption>${weather[0]["description"]}</figcaption>
-            </figure>` ;
+            </figure>
+            ` ;
             li.innerHTML = markup
-            list.appendChild(li);
+            list.appendChild(li)
             msg.innerText = ""
         })
         .catch(()=>{
             msg.innerText = "Search for a valid city"
         })
+    input.value = ""
 })
